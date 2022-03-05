9fbe2260a53ff75632fb45d67f9c914fc7890896
taiga-n <22574261+taiga-n@users.noreply.github.com>
2022-03-06 00:20:21 +0900

    first commit

---
+++ Unity_Sound_Utility/AutoDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public float destroyTime = 10f;
+   
+    void Start()
+    {
+        StartCoroutine(DestroySelf());
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Unity_Sound_Utility/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static void Play(string name)
+    {
+        //空のゲームオブジェクトgoを作成し、名前を"TempAudioSource"
+        GameObject go = new GameObject("TempAudioSource");
+
+        //AudioSourceをgoに追加して、AudioClipを指定し再生
+        AudioSource audioSource = go.AddComponent<AudioSource>();
+        AudioClip clip = Resources.Load<AudioClip>("Sounds/" + name);
+        audioSource.clip = clip;
+        audioSource.Play();
+
+        //AutoDestroyをgoに追加して、AudioClipの長さと同じ時間経過後に自分を削除させる。
+        AutoDestroy autoDestroy = go.AddComponent<AutoDestroy>();
+        autoDestroy.destroyTime = clip.length;
+    }
+}
